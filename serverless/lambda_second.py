ab05964988270b23cea01394c8d52c5d5a04e3cf
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2025-11-27 23:11:25 +0900

    【コード変更検証】実行時間延長&エラースロー

--- serverless/lambda_second.py
+++ serverless/lambda_second.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import random
+import time
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,4 +11,9 @@ def handler(event, context):
     logger.info("Event: %s", json.dumps(event))
 
     logger.info("Second!")
+
+    if random.random() < 0.3:
+        time.sleep(2)
+        raise Exception("Random failure occurred in lambda_second")
+
     return {"statusCode": 200, "body": "Hello from Lambda!"}

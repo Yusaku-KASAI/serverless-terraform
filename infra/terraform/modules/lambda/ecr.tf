e7b67e9a1342fcc2f124c6c97dd1224ce1e0960b
Yusaku-KASAI <kasai.yusaku.work@gmail.com>
2026-01-16 17:09:26 +0900

    【モジュール修正】ECRリポジトリポリシー付与削除微修正

--- infra/terraform/modules/lambda/ecr.tf
+++ infra/terraform/modules/lambda/ecr.tf
@@ -51,9 +51,6 @@ data "aws_iam_policy_document" "ecr_pull" {
     actions = [
       "ecr:BatchGetImage",
       "ecr:GetDownloadUrlForLayer",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-      "ecr:GetRepositoryPolicy",
     ]
 
     condition {
